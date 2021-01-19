898eb0b203400a546cbb506513d751da51c79b6e
C0V1D-19 <deathbydickey@gmail.com>
2021-01-19 07:54:30 -0600

    Create install.cmd

---
+++ install.cmd
@@ -0,0 +1,9 @@
+@echo off
+pip install discord.py
+echo discord py installed
+pip install -U discord.py[voice]
+echo discord py voice installed 
+pip install PyNaCl
+echo PyNaC1 installed
+echo done
+pause
