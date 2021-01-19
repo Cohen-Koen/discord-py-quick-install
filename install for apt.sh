37b21fde9dce850a8ba86ff0c8f9c6f6eef08022
C0V1D-19 <deathbydickey@gmail.com>
2021-01-19 17:41:19 -0600

    uhhhh

--- install for apt.sh
+++ install for apt.sh
@@ -1,6 +1,12 @@
 echo off
+sudo apt update
 sudo apt get-install python3
+sudo apt install python3-pip
 python3 -m pip install -U discord.py
 echo discord py installed
 python3 -m pip install -U "discord.py[voice]"
 echo voice support added
+echo If you see E: its because you dont have python installed or pip is being annoying.....\\
+again
+echo 'sorry if there are issues as I have never coded shell script or batch and Im trying my best guys'
+
